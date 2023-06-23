a23c8f0bff2917cbaf50e3078f33822ff1abfc76
ColtenOuO <jun930436@gmail.com>
2023-06-24 01:14:26 +0800

    Accepted.

--- codeforces/Codeforces-Round-345-Div1/pD.cpp
+++ codeforces/Codeforces-Round-345-Div1/pD.cpp
@@ -1,15 +1,15 @@
 #include <bits/stdc++.h>
-
+ 
 #define int long long
-
+ 
 using namespace std;
-
+ 
 int bit[800005][3],arr[400005],pos[400005],val[400005];
 vector<pair<int,int>> queries[400005];
 int dp1[400005],dp2[400005],dp3[400005],dp4[400005];
 vector<int> a,b;
-int ans[400005],x[400005],y[400005],cnt[400005];
-
+int ans[400005],x[400005],y[400005],cnt[400005],r[400005];
+ 
 void update(int id,int idx,int val)
 {
 	while( idx <= 8e5 ) bit[idx][id] = max(bit[idx][id],val) , idx += ( idx & -idx );
@@ -54,12 +54,11 @@ signed main()
 		int front_dp_mx = query(1,a[i]-1);
 		dp1[i] = max(now_dp_mx,front_dp_mx+1);
 		dp2[i] = front_dp_mx + 1;
-		cnt[front_dp_mx+1]++;
 		
 		for( auto k : queries[i+1] )
 		{
 			ans[k.second] = query(1,k.first-1) + 1;
-			x[k.second] = ans[k.second];
+			x[k.second] = query(1,k.first-1) + 1;
 		}
 		
 		now_dp_mx = max(now_dp_mx,front_dp_mx+1);
@@ -68,8 +67,12 @@ signed main()
 		lis = max(lis,now_dp_mx);
 	}
 	
-	now_dp_mx = 1;
+	//for(int i=0;i<n;i++) cout << a[i] << " ";
+	//cout << "\n";
+	//for(int i=0;i<n;i++) cout << abs((int)b.size()-a[i]) + 1 << " ";
+	//cout << "\n";
 	
+	now_dp_mx = 1;
 	for(int i=n-1;i>=0;i--)
 	{
 		int save = a[i];
@@ -78,15 +81,15 @@ signed main()
 		dp3[i] = max(now_dp_mx,back_dp_mx+1);
 		dp4[i] = back_dp_mx + 1;
 		
+		if( dp2[i] + dp4[i] - 1 == lis ) cnt[dp4[i]]++;
+		
+	//	cout << i << " " << a[i] << " " << dp2[i] << " " << dp4[i] << " " << cnt[dp4[i]] << "\n";
+		
 		for( auto k : queries[i+1] )
 		{
-			if( k.first >= save )
-			{
-				int num = abs((int)b.size()-k.first) + 1;
-				int ouo = ans[k.second] + query(2,num-1);
-				ans[k.second] = ouo;
-				y[k.second] = query(2,num-1) + 1;
-			}
+			int num = abs((int)b.size()-k.first) + 1;
+			ans[k.second] = max(ans[k.second],query(2,num-1)+1);
+			y[k.second] = query(2,num-1) + 1;
 		}
 		
 		now_dp_mx = dp3[i];
@@ -94,6 +97,8 @@ signed main()
 		
 		a[i] = save;
 	}
+	
+	//cout << "\n";
 	
 	for(int i=0;i<n;i++)
 	{
@@ -110,8 +115,17 @@ signed main()
 			if( i != n - 1 ) ans[k.second] = max(ans[k.second],dp3[i+1]);
 			
 			ans[k.second] = max(ans[k.second],x[k.second]+y[k.second]-1);
-			if( cnt[dp2[i]] == 1 ) ans[k.second] = max(ans[k.second],lis-1);
+			//if( cnt[dp2[i]] == 1 ) ans[k.second] = max(ans[k.second],lis-1);
 			//else ans[k.second] = max(ans[k.second],lis);
+			
+		//	cout << i << " " << cnt[dp4[i]] << "\n";
+			
+			if( cnt[dp4[i]] != 1 ) ans[k.second] = max(ans[k.second],lis);
+			else ans[k.second] = max(ans[k.second],lis-1);
+			
+		//	cout << x[k.second] << " " << y[k.second] << "\n";
+			
+			ans[k.second] = max(ans[k.second],x[k.second]+y[k.second]-1);
 		}
 	}
 	
