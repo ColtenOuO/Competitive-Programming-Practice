d36f8dad48af6ec58520f41be91dbff4ac301488
ColtenOuO <jun930436@gmail.com>
2023-06-27 01:22:00 +0800

    accepted.

--- codeforces/Codeforces-Round-867-Div3/pF.cpp
+++ codeforces/Codeforces-Round-867-Div3/pF.cpp
@@ -1,74 +1,86 @@
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
+
 #define int long long
 
 using namespace std;
 
-vector<int> a[200005];
-int dp[200005],ans = 1e9,sum = 0,cost;
+vector<int> e[200005];
+int ans = -1e9,n,k,c;
+multiset<int> dp[200005];
+map<pair<int,int>,int> mp;
 
-void dfs1(int idx,int last)
+void dfs(int idx,int last)
 {
-    for( auto i : a[idx] )
-    {
-        if( i == last ) continue;
-        dfs1(i,idx);
-
-        dp[idx] += dp[i];
-    }
-
-    dp[idx]++;
-    if( idx != 1 ) sum += dp[idx];
-    //cout << idx << " " << dp[idx] << "\n";
+	int ok = 0;
+	for( auto i : e[idx] )
+	{
+		if( i == last ) continue;
+		dfs(i,idx);
+		ok++;
+		if( dp[i].size() != 0 ) dp[idx].insert( *(dp[i]).rbegin() + k ) , mp[{idx,i}] = *(dp[i]).rbegin() + k;
+	}
+	
+	if( ok == 0 ) dp[idx].insert(0);
+	
+	return;
 }
-void dfs2(int idx,int last,int c)
+void dfs2(int idx,int last,int cost)
 {
-    for( auto i : a[idx] )
-    {
-        if( i == last ) continue;
-
-        sum -= dp[i];
-        dp[idx] -= dp[i];
-        sum += dp[idx];
-        
-        ans = max(ans,sum-c);
-
-        dfs2(i,idx,c+cost);
-
-        sum -= dp[idx];
-        dp[idx] += dp[i];
-        sum += dp[i];
-    }
-
-    return;
+	int num = -1;
+	if( dp[idx].size() != 0 ) num = *(dp[idx]).rbegin();
+	if( num != -1 ) ans = max(ans,num-cost);
+	
+	//cout << idx << " " << num << "\n";
+	
+	for( auto i : e[idx] )
+	{
+		if( i == last ) continue;
+		
+		int mp_save = mp[{idx,i}];
+		dp[idx].erase( dp[idx].find(mp[ {idx,i} ])  );
+		int save = -1;
+		
+		if( dp[idx].size() != 0 )
+		{
+			save = *(dp[idx]).rbegin();
+			dp[i].insert(save+k);	
+			mp[{ i , idx}] = save + k;
+		}
+		
+		dfs2(i,idx,cost+c);
+		
+		if( dp[idx].size() != 0 )
+		{
+			mp.erase({i,idx});
+			dp[i].erase( dp[i].find(save+k) );
+		}
+		
+		dp[idx].insert( mp_save );
+	}
+	
+	return;
 }
 signed main(void)
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
-
-    int q;
-    cin >> q;
-
-    while(q--)
-    {
-        ans = 1e9 , sum = 0;
-        int n,m;
-        cin >> n >> m >> cost;
-        for(int i=1;i<=n;i++) a[i].clear() , dp[i] = 0;
-        for(int i=0;i<m;i++)
-        {
-            int x,y;
-            cin >> x >> y;
-            a[x].push_back(y);
-            a[y].push_back(x);
-        }
-
-        dfs1(1,0);
-        ans = sum;
-        cout << ans << "\n";
-        dfs2(1,0,0);
-
-        cout << ans << "\n";
-    }
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	
+	int q;
+	cin >> q;
+	while(q--)
+	{
+		cin >> n >> k >> c;
+		for(int i=1;i<=n;i++) e[i].clear() , dp[i].clear();
+		for(int i=0;i<n-1;i++)
+		{
+			int x,y;
+			cin >> x >> y;
+			e[x].emplace_back(y) , e[y].emplace_back(x);
+		}
+		ans = -1e9;
+		dfs(1,0);
+		dfs2(1,0,0);
+		
+		cout << ans << "\n";
+	}
 }
